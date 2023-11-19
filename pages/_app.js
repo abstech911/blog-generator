b4629540c2244f526e97e0f28c5d1edffd38e4a7
Abubakar Sani <abstech911@gmail.com>
2023-11-19 18:08:02 +0100

    Fixed

--- pages/_app.js
+++ pages/_app.js
@@ -3,27 +3,27 @@ import {UserProvider} from "@auth0/nextjs-auth0/client";
 import {DM_Sans, DM_Serif_Display} from '@next/font/google';
 
 const dmSans = DM_Sans({
-    weight:['400', '500', '700']
-    , subsets:['latin']
-    , variable:"--font-dm-sans"
+    weight: ['400', '500', '700']
+    , subsets: ['latin']
+    , variable: "--font-dm-sans"
 })
 const dmSerifDisplay = DM_Serif_Display({
-    weight:['400']
-    , subsets:['latin']
-    , variable:"--font-dm-serif"
+    weight: ['400']
+    , subsets: ['latin']
+    , variable: "--font-dm-serif"
 })
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps}) {
     const getLayout = Component.getLayout || ((page) => {
         return page
     })
-  return (
-      <UserProvider>
-          <main className={`${dmSans.variable} ${dmSerifDisplay.variable} font-body`}>
-          {getLayout(<Component {...pageProps} />, pageProps)}
-          </main>
-      </UserProvider>
-  )
+    return (
+        <UserProvider>
+            <main className={`${dmSans.variable} ${dmSerifDisplay.variable} font-body`}>
+                {getLayout(<Component {...pageProps} />, pageProps)}
+            </main>
+        </UserProvider>
+    )
 }
 
 export default MyApp
